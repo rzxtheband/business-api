01abc5a41a17a0042b0fd607ed59bcee4a75fdab
rzxtheband <84014007+rzxtheband@users.noreply.github.com>
2024-07-30 20:47:25 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create the connection pool
 const pool = mysql.createPool({
-  host: 'srv922.hstgr.io', // Replace with your MySQL host
-  user: 'u832555779_SchoolAdmin', // Your MySQL user
-  password: 'SchoolAdminPassword@123', // Your MySQL password
-  database: 'u832555779_school_api', // Your MySQL database
+  host: 'srv1336.hstgr.io', // Replace with your MySQL host
+  user: 'u832555779_business_api', // Your MySQL user
+  password: 'BusinessApiAdmin@123', // Your MySQL password
+  database: 'u832555779_business_api', // Your MySQL database
   waitForConnections: true,
   connectionLimit: 10, // Number of connections in the pool
   queueLimit: 0, // No limit to the queue size
